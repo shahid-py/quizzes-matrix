a95e1c6f5b3dae6a1aebfa9991327d28cb77c4d4
Mohd Shahid Khan <khan42290@gmail.com>
2022-04-04 12:44:18 +0530

    feat: created timer of quiz , constants of api,quizinfo

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const API_URL = "https://quiz-backend.shahid-v2.repl.co";
+export const quizInfo =  [
+  "This quiz contains total 5 questions",
+  "Each question carries 5 marks for correct answer and -2 marks for wrong answer",
+]
